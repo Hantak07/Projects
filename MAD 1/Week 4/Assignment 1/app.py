bdc934b8b46e9eef808d8b557a025106ac010f12
Hantak07 <76593073+Hantak07@users.noreply.github.com>
2024-10-16 05:30:52 +0530

    html files update

--- MAD 1/Week 4/Assignment 1/app.py
+++ MAD 1/Week 4/Assignment 1/app.py
@@ -1,9 +1,13 @@
+from crypt import methods
+
 from flask import Flask
 
 app = Flask(__name__, template_folder='templates')
 
-
-
+@app.route("/index", methods=['POST'])
+def index():
+    page = ""
+    pass
 
 
 if __name__ == '__main__':
