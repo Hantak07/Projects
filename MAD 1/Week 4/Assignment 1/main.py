edd96e40c878e624d2e9781ee44a3bb857c52cd5
Hantak07 <76593073+Hantak07@users.noreply.github.com>
2024-10-26 19:18:26 +0530

    Database added

--- MAD 1/Week 4/Assignment 1/main.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt
-from flask import Flask, request, render_template
-
-app = Flask(__name__, template_folder='templates')
-
-@app.route('/', methods=['GET', 'POST'])
-def main_page():
-    if request.method == "GET":
-        pass
